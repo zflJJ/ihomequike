d01131e6822033b3346531afb873c4009942b441
unknown <101@qhieco.com>
2018-06-07 16:54:55 +0800

    处理车辆未入场

--- src/components/reservationDt/reservationInfo.vue
+++ src/components/reservationDt/reservationInfo.vue
@@ -220,7 +220,9 @@ export default {
           if(res.body.data==null||res.body.data==''||res.body.data==undefined){
             this.getOrder()
           }else if(res.body.data.state == 1301){
-            if(res.body.data.parkingState){
+           if(res.body.data.parkingState==null){
+             this.getOrder()
+           }else if(res.body.data.parkingState){
               if(res.body.data.parkingState == 1302){
                 localStorage.setItem("orderId",res.body.data.orderParkingId)
                 this.$router.push('reservationPaking');
@@ -228,6 +230,7 @@ export default {
                 this.$router.push('reservationOld');
               }
             }
+
             this.lockId = null;
             this.desX = res.body.data.lng;
             this.desY = res.body.data.lat;
